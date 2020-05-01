a998f9abf07574bded00a7f1b974dd600b54156c
Luan Lopes <luanglopes@gmail.com>
2020-05-01 20:37:42 -0300

    configure knex

---
+++ src/database/knexfile.ts
@@ -0,0 +1,53 @@
+require('ts-node/register')
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'node_api_objection',
+    },
+    migrations: {
+      tableName: 'migrations',
+      extension: 'ts',
+    },
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'node_api_objection',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'migrations',
+      extension: 'ts',
+    },
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'node_api_objection',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'migrations',
+      extension: 'ts',
+    },
+  },
+}
