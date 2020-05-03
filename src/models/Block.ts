101ff866b2c8099e501e4333bb64ecbd65457e0b
Luan Lopes <luanglopes@gmail.com>
2020-05-03 01:36:18 -0300

    adds images to blocks

--- src/models/Block.ts
+++ src/models/Block.ts
@@ -1,7 +1,30 @@
-import { Model } from 'objection'
+import { Model, RelationMappings } from 'objection'
+
+import databaseHelpers from '../database/helpers'
 
 export default class Block extends Model {
   static get tableName(): string {
     return 'blocks'
+  }
+
+  static get relationMappings(): RelationMappings {
+    const ImageModelPath = databaseHelpers.importModelPath('Image')
+
+    return {
+      images: {
+        relation: Model.HasOneRelation,
+        modelClass: ImageModelPath,
+        filter(builder): void {
+          builder.where('imageableType', 'block_image')
+        },
+        beforeInsert(model): void {
+          model.imageableType = 'block_image'
+        },
+        join: {
+          from: 'blocks.id',
+          to: 'images.imageableId',
+        },
+      },
+    }
   }
 }
