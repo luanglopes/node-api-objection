0cce6a1ef0d26ad3d94a145e110ef2fd2e8ee1b7
Luan Lopes <luanglopes@gmail.com>
2020-05-03 00:18:53 -0300

    configure knex mappers

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import { Model } from 'objection'
+import { Model, knexSnakeCaseMappers } from 'objection'
 import Knex from 'knex'
 
 import knexConfig from './knexfile'
@@ -6,6 +6,6 @@ import knexConfig from './knexfile'
 const environment = process.env.NODE_ENV || 'development'
 const connectionConfig: Knex.Config = knexConfig[environment]
 
-const knex = Knex(connectionConfig)
+const knex = Knex({ ...connectionConfig, ...knexSnakeCaseMappers })
 
 Model.knex(knex)
